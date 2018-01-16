ddaf2b796248d08faf39e99f37c7251248ff2373
wei <1306500394@qq.com>
2018-01-16 21:47:48 +0800

    test second

---
+++ app/src/main/java/com/angel/willy/Base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.angel.willy.Base;
+
+import android.support.v7.app.AppCompatDialogFragment;
+
+/**
+ * Created by wei on 2018/1/15.
+ */
+
+public abstract class BaseFragment extends AppCompatDialogFragment {
+}
